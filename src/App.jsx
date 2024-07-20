43044fe1ff16604f61254aea881fde2b776c33dc
Mongfer <90517849+mongfer450@users.noreply.github.com>
2024-07-21 01:30:58 +0700

    add more

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Pcal from "./components/Pcal";
+
+const App = () => {
+  
+  return (
+    <div>
+      <Navbar />
+      <Pcal />
+
+      
+    </div>
+  )
+
+}
+
+export default App;
---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dropdown = () => {
+  return (
+    <div>
+        Dropdown
+        {/* waiting for update */}
+    </div>
+  );
+}
+export default Dropdown
